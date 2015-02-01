9de85fda68675d64f035248eccd3f6db394fc4bf
Chris Truman <cleetruman@gmail.com>
2015-01-31 22:41:16 -0800

    Commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+
+link_with 'QuickControl', 'Control'
+pod 'upnpx', :path => '../upnpx' #'1.3.1'
+pod 'AutoCoding', '~> 2.2'
+
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AutoCoding
+#define COCOAPODS_POD_AVAILABLE_AutoCoding
+#define COCOAPODS_VERSION_MAJOR_AutoCoding 2
+#define COCOAPODS_VERSION_MINOR_AutoCoding 2
+#define COCOAPODS_VERSION_PATCH_AutoCoding 1
+
+// upnpx
+#define COCOAPODS_POD_AVAILABLE_upnpx
+#define COCOAPODS_VERSION_MAJOR_upnpx 1
+#define COCOAPODS_VERSION_MINOR_upnpx 3
+#define COCOAPODS_VERSION_PATCH_upnpx 1
+
